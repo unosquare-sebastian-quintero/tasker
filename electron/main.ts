16d698ceae0451da71fd66f242bfa1d97d17453e
Sebastian G. Q <sebastian.quintero@unosquare.com>
2023-09-18 15:31:56 -0500

    feat: base tray

--- electron/main.ts
+++ electron/main.ts
@@ -1,7 +1,19 @@
-import { BrowserWindow, Menu, Tray, app, nativeImage } from "electron";
+import { BrowserWindow, Tray, app, nativeImage } from "electron";
 
-function createWindow() {
-  var window = new BrowserWindow({ title: "Main Window" });
+function createRichContextualMenu(x: number, y: number) {
+  const width = 640;
+  var window = new BrowserWindow({
+    alwaysOnTop: true,
+    focusable: true,
+    movable: false,
+    resizable: false,
+    frame: false,
+    x: x - width / 2,
+    y,
+    width,
+  });
+
+  window.addListener("blur", () => window.close());
 
   if (process.env["VITE_DEV_SERVER_URL"]) {
     window.loadURL(process.env["VITE_DEV_SERVER_URL"]);
@@ -10,48 +22,18 @@ function createWindow() {
   }
 }
 
-function createRichContextualMenu() {
-  var window = new BrowserWindow({
-    alwaysOnTop: true,
-    focusable: true,
-    movable: false,
-    resizable: false,
-    frame: false,
-  });
-}
-
 app.whenReady().then(function ready() {
   const icon = nativeImage.createFromPath("assets/vite.svg");
 
-  const tray = new Tray(icon);
-  const contextMenu = Menu.buildFromTemplate([
-    {
-      label: "Item1",
-      type: "normal",
-    },
-    {
-      label: "Item2",
-      type: "checkbox",
-    },
-    {
-      label: "Item3",
-      type: "radio",
-    },
-    {
-      label: "Item4",
-      type: "separator",
-    },
-  ]);
+  var tray = new Tray(icon);
 
-  // contextMenu.append({ label: "Item3" });
-  const bounds = tray.getBounds();
-  console.log({ bounds });
-  // tray.addListener("click", () => {
-  //   createWindow();
-  // });
-  tray.setContextMenu(contextMenu);
-  tray.setToolTip("Timesheet");
+  tray.setToolTip("Create task and track time");
   tray.setTitle("Timesheet");
 
-  // createWindow();
+  tray.addListener("click", () => {
+    const { x, y, width, height } = tray.getBounds();
+    createRichContextualMenu(x + width / 2, y + height);
+  });
 });
+
+app.on("window-all-closed", () => {});
