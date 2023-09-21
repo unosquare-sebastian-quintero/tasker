8b2d678be537c048d3f37eff36a2b14e715d7eea
Sebastian G. Q <sebastian.quintero@unosquare.com>
2023-09-21 15:37:12 -0500

    refactor: global stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./app";
-import "./style.scss";
+import "./global.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
