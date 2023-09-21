fcdd7801478e96274a242e62708b098f4c60d0d8
Sebastian G. Q <sebastian.quintero@unosquare.com>
2023-09-21 14:06:56 -0500

    chore: storybook and axe

--- src/components/button.test.tsx
+++ src/components/button.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import { axe } from "vitest-axe";
 import { Button } from "./button";
 
 test("first", () => {
@@ -7,4 +8,10 @@ test("first", () => {
   const button = screen.getByRole("button");
 
   expect(button).toBeInTheDocument();
+});
+
+test("accessibility", async () => {
+  const { container } = render(<Button title="hello" />);
+
+  expect(await axe(container)).toHaveNoViolations();
 });
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,10 @@
 import "@testing-library/jest-dom/vitest";
+import { AxeMatchers } from "vitest-axe";
+import * as matchers from "vitest-axe/matchers";
+
+expect.extend(matchers);
+
+declare module "vitest" {
+  export interface Assertion extends AxeMatchers {}
+  export interface AsymmetricMatchersContaining extends AxeMatchers {}
+}
